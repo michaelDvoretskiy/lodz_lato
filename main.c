0fd177668092a71a8f7849853201e939b1d825b1
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-05 09:24:19 +0200

    read chain of characters

--- main.c
+++ main.c
@@ -3,7 +3,18 @@
 int main() {
     char file_name[31];
     printf("Enter name:\n");
-    scanf("%30s", file_name);
+
+    int i = 0;
+    char c;
+    scanf("%c", &c);
+    while (c != '\n') {
+        file_name[i] = c;
+        // or *(file_name + i) = c;
+        scanf("%c", &c);
+        i++;
+    }
+    file_name[i] = '\0';
+    // or *(file_name + i) = '\0';
 
     printf("You entered: %s", file_name);
     return 0;

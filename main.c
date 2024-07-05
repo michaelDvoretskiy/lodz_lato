a1ff47db3276d34e70a4394ea23d3971feae55ed
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-05 09:17:42 +0200

    output chain of characters

--- main.c
+++ main.c
@@ -3,6 +3,22 @@
 int main() {
     char message[31] = "Hello world\0";
 
-    printf("The message is: %s", message);
+    int i = 0;
+    // with []
+    printf("The message is: ");
+    while (message[i] != '\0') {
+        printf("%c", message[i]);
+        i++;
+    }
+    printf("\n");
+
+    // without []
+    printf("The message is: ");
+    i = 0;
+    while (*(message+i) != '\0') {
+        printf("%c", *(message+i));
+        i++;
+    }
+
     return 0;
 }

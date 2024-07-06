609779c5085e3f7a573333758ac9208c8e9fac55
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-06 17:54:21 +0200

    create string based on int number

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    scanf("%30s", file_name);
+    char* baseString = "Hello\0";
+    char* result = malloc(10);
+    for (int baseNumber = 0; baseNumber < 10; baseNumber++) {
+        sprintf(result, "%s %d !", baseString, baseNumber);
 
-    printf("You entered: %s", file_name);
+        printf("%s\n", result);
+    }
     return 0;
 }

63cd375000b8753500133d97c3e987b1a4af7c38
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-06 17:36:43 +0200

    write int number to the text file

--- main.c
+++ main.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    scanf("%30s", file_name);
+    FILE* outFileOdd = fopen("out-odd.txt", "w");
+    FILE* outFileEven = fopen("out-even.txt", "w");
+    if (outFileOdd == NULL || outFileEven == NULL) {
+        printf("File open error");
+        if (outFileOdd != NULL) {
+            fclose(outFileOdd);
+        }
+        if (outFileEven != NULL) {
+            fclose(outFileEven);
+        }
+        return 1;
+    }
+    int values[] = {1, 5, 6, 8, 2, 13};
+    int arrSize = 6;
+    for (int i = 0; i < arrSize; i++) {
+        if (values[i] % 2 == 0) {
+            fprintf(outFileEven, "%d\n", *(values+i));
+        } else {
+            fprintf(outFileOdd, "%d\n", *(values+i));
+        }
+    }
 
-    printf("You entered: %s", file_name);
+    fclose(outFileEven);
+    fclose(outFileOdd);
+
     return 0;
 }

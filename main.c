27b86b3450ff628faca971afa11d850a06bdd89d
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-07 16:32:02 +0200

    read all numbers from file, if not eof - corrupted

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    scanf("%30s", file_name);
+    char file_name[31] = "input.txt\0";
 
-    printf("You entered: %s", file_name);
+    FILE* my_file = fopen(file_name, "r");
+    int number_from_file;
+    while (fscanf(my_file, "%d", &number_from_file) == 1) {
+    }
+    if (!feof(my_file)) {
+        printf("File corrupted\n");
+        return 1;
+    }
+    fclose(my_file);
+
     return 0;
 }

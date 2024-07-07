a8eae22745b657a10357ab62da723f358c5d93e7
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-07 18:04:41 +0200

    check number of numbers in file. all values has to be a numbers

--- main.c
+++ main.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    scanf("%30s", file_name);
+    char file_name[31] = "input.txt\0";
+    FILE* f = fopen(file_name, "r");
+    if (f == NULL) {
+        printf("cannot open file\n");
+        return 1;
+    }
 
-    printf("You entered: %s", file_name);
+    int number_from_file;
+    int numbers_in_file = 0;
+    int has_to_be_numbers_in_file = 10;
+    while (fscanf(f, "%d", &number_from_file) == 1) {
+        numbers_in_file++;
+    }
+    if (numbers_in_file != has_to_be_numbers_in_file) {
+        printf("corrupted\n");
+        return 1;
+    }
+    if (!feof(f)) {
+        printf("corrupted\n");
+        return 1;
+    }
+
+    printf("all is ok!\n");
+    fclose(f);
+
     return 0;
 }

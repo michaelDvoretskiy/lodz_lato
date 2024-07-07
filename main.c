3996845c5826570297c72efa0394c6dfa9dee617
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-07 16:18:04 +0200

    free memory after malloc

--- main.c
+++ main.c
@@ -11,10 +11,16 @@ int main() {
         printf("no such file %s\n", file_name);
     } else {
         printf("file %s was opened successfully\n", file_name);
+        fclose(my_file);
     }
-    fclose(my_file);
+
 
     char* file_name2 = malloc(31);
+    if (file_name2 == NULL) {
+        printf("Couldn't allocate memory\n");
+        return 1;
+    }
+
     printf("Enter file name to create:\n");
     scanf("%30s", file_name2);
 
@@ -23,8 +29,9 @@ int main() {
         printf("cannot create file %s\n", file_name2);
     } else {
         printf("file %s was created successfully\n", file_name);
+        fclose(my_file2);
     }
-    fclose(my_file2);
+    free(file_name2);
 
     return 0;
 }

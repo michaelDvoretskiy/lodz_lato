758e90b065f7037f3c01fb517252d3a6474e946a
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-07 15:54:05 +0200

    generate 10 random int values between 0 and 1000

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    scanf("%30s", file_name);
+    int res;
+    srand(time(NULL));
+    for (int i = 0; i < 10; i++) {
+        res = rand() % 1001;
+        printf("%d\n", res);
+    }
 
-    printf("You entered: %s", file_name);
     return 0;
 }

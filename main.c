9c6869965a821f81a58bb8a13dbe743d3db1d08a
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-10 11:35:26 +0200

    set '\0' in the end of the line

--- main.c
+++ main.c
@@ -1,23 +1,5 @@
 #include <stdio.h>
 
-int read_file(char* filename);
-
-int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    if (scanf("%30s", file_name) != 1) {
-        printf("failed to read the filename\n");
-        return 1;
-    }
-
-    int res = read_file(file_name);
-    if (res != -1) {
-        printf("File quantity %d", res);
-    }
-
-    return 0;
-}
-
 void change_n_to_0(char* arr, int max_len) {
     int i = 0;
     while (*(arr+i) != '\r' && *(arr+i) != '\n' && i < max_len) {
@@ -26,28 +8,21 @@ void change_n_to_0(char* arr, int max_len) {
     *(arr+i) = '\0';
 }
 
-int read_file(char* filename) {
-    int files_count = 0;
-    if (filename == NULL) {
-        return -1;
-    }
-    FILE* f = fopen(filename, "r");
+int main() {
+    char file_name[31] = "file1.txt\0";
+
+    FILE* f = fopen(file_name, "r");
     if (f == NULL) {
         return 0;
     }
-    char inner_filename[31];
-    while (fgets(inner_filename, sizeof(inner_filename), f)) {
-        change_n_to_0(inner_filename, 30);
 
-        printf("%s\n", inner_filename);
-        files_count++;
-
-        int res = read_file(inner_filename);
-        if (res != -1) {
-            files_count += res;
-        }
+    char file_line[31];
+    while (fgets(file_line, sizeof(file_line), f)) {
+        change_n_to_0(file_line, 30);
+        printf("%s\n", file_line);
     }
+
     fclose(f);
 
-    return files_count;
+    return 0;
 }

c067b3c1e7fb0225c88c292c5450f949e1a05bad
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-10 11:21:27 +0200

    recursive files open, read next filename in previous file

--- main.c
+++ main.c
@@ -1,10 +1,53 @@
 #include <stdio.h>
 
+int read_file(char* filename);
+
 int main() {
     char file_name[31];
     printf("Enter name:\n");
-    scanf("%30s", file_name);
+    if (scanf("%30s", file_name) != 1) {
+        printf("failed to read the filename\n");
+        return 1;
+    }
 
-    printf("You entered: %s", file_name);
+    int res = read_file(file_name);
+    if (res != -1) {
+        printf("File quantity %d", res);
+    }
+
     return 0;
+}
+
+void change_n_to_0(char* arr, int max_len) {
+    int i = 0;
+    while (*(arr+i) != '\r' && *(arr+i) != '\n' && i < max_len) {
+        i++;
+    }
+    *(arr+i) = '\0';
+}
+
+int read_file(char* filename) {
+    int files_count = 0;
+    if (filename == NULL) {
+        return -1;
+    }
+    FILE* f = fopen(filename, "r");
+    if (f == NULL) {
+        return 0;
+    }
+    char inner_filename[31];
+    while (fgets(inner_filename, sizeof(inner_filename), f)) {
+        change_n_to_0(inner_filename, 30);
+
+        printf("%s\n", inner_filename);
+        files_count++;
+
+        int res = read_file(inner_filename);
+        if (res != -1) {
+            files_count += res;
+        }
+    }
+    fclose(f);
+
+    return files_count;
 }

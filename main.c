399ae1a4e6cbb292d9230f8e374ba4ebd7b71858
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-13 15:34:43 +0200

    create 'point' structure

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    scanf("%30s", file_name);
+struct point_t {
+    int x;
+    int y;
+};
 
-    printf("You entered: %s", file_name);
+int main() {
+    struct point_t p1, p2;
+    p1.x = 10;
+    p1.y = 20;
+    p2 = p1;
+
+    // подальші зміни полів  однієї структури
+    // не впливають на дані іншої структури
+    p2.x = 1;
+    p1.y = 2;
+    printf("p1 x=%d, y=%d\n", p1.x, p1.y);
+    printf("p2 x=%d, y=%d\n", p2.x, p2.y);
     return 0;
 }

71082cc4ad227a7d63adcb541172b506ceff43f2
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-13 16:21:15 +0200

    change structure fields inside a function

--- main.c
+++ main.c
@@ -5,16 +5,15 @@ struct point_t {
     int y;
 };
 
-void show_point(struct point_t* p) {
-    printf("Point info\n");
-    printf("x = %d, y = %d", p->x, p->y);
+void set_values(struct point_t* p, int x, int y) {
+    p->x = x;
+    p->y = y;
 }
 
 int main() {
     struct point_t p1;
-    p1.x = 10;
-    p1.y = 20;
-    show_point(&p1);
+    set_values(&p1, 5, 6);
+    printf("x=%d, y=%d", p1.x, p1.y);
 
     return 0;
 }

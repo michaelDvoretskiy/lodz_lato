a6e3a0542d6b051cb6708798d262e956e2f13bfc
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-13 16:25:20 +0200

    return pointer to the structure from function

--- main.c
+++ main.c
@@ -5,15 +5,18 @@ struct point_t {
     int y;
 };
 
-void set_values(struct point_t* p, int x, int y) {
+struct point_t* set_values(struct point_t* p, int x, int y) {
     p->x = x;
     p->y = y;
+
+    return p;
 }
 
 int main() {
     struct point_t p1;
-    set_values(&p1, 5, 6);
-    printf("x=%d, y=%d", p1.x, p1.y);
-
+    struct point_t* p2 = set_values(&p1, 5, 6);
+    // p2 буде вказівником на p1, фактично це одна і та ж структура
+    printf("x=%d, y=%d\n", p1.x, p1.y);
+    printf("x=%d, y=%d\n", p2->x, p2->y);
     return 0;
 }

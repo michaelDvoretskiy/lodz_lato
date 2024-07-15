251a49a0f9e867565336b54df4a067145b4dde59
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-15 09:47:19 +0200

    read point structure from the binary file

--- main.c
+++ main.c
@@ -5,16 +5,16 @@ struct point_t {
     int y;
 };
 
-int write_point_to_binary_file(struct point_t* p, char* filename) {
+int read_point_from_binary_file(struct point_t* p, char* filename) {
     if (filename == NULL || p == NULL) {
         return 1;
     }
-    FILE* f = fopen(filename, "wb");
+    FILE* f = fopen(filename, "rb");
     if (f == NULL) {
         return 2;
     }
 
-    int res = fwrite(p, sizeof(struct point_t), 1, f);
+    int res = fread(p, sizeof(struct point_t), 1, f);
     if (res != 1) {
         fclose(f);
         return 3;
@@ -26,11 +26,11 @@ int write_point_to_binary_file(struct point_t* p, char* filename) {
 
 int main() {
     struct point_t p;
-    p.x = 10;
-    p.y = 20;
-    int save_res = write_point_to_binary_file(&p, "saved_point.bin\0");
+    p.x = 1;
+    p.y = 2;
+    int save_res = read_point_from_binary_file(&p, "saved_point.bin\0");
     if (save_res == 0) {
-        printf("Saved\n");
+        printf("x=%d, y=%d\n", p.x, p.y);
     } else {
         printf("Smth went wrong\n");
     }

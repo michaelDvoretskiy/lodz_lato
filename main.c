2fe89f6c3bd1d1dbac73d607361c7edf6d984ae5
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-15 09:22:26 +0200

    read point structure fields from text file

--- main.c
+++ main.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
 
-int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    scanf("%30s", file_name);
+struct point_t {
+    int x;
+    int y;
+};
 
-    printf("You entered: %s", file_name);
+int read_point_from_text_file(struct point_t* p, char* filename) {
+    if (filename == NULL || p == NULL) {
+        return 1;
+    }
+    FILE* f = fopen(filename, "r");
+    if (f == NULL) {
+        return 2;
+    }
+
+    int res = fscanf(f, "%d %d", &(p->x), &(p->y));
+    if (res != 2) {
+        fclose(f);
+        return 3;
+    }
+    fclose(f);
+
+    return 0;
+}
+
+int main() {
+    struct point_t p;
+    p.x = 10;
+    p.y = 20;
+    int save_res = read_point_from_text_file(&p, "saved_point.txt\0");
+    if (save_res == 0) {
+        printf("x=%d, y=%d\n", p.x, p.y);
+    } else {
+        printf("Smth went wrong\n");
+    }
     return 0;
 }

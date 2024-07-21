31efd056fed328e690b563140f1c220a9eb2fbd3
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-21 20:30:29 +0200

    declare structure in the separate file

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "student.h"
 
 int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    scanf("%30s", file_name);
+    struct student_t s;
+    s.name = malloc(20);
+    strcpy(s.name, "Vasya");
+    s.number = 123;
 
-    printf("You entered: %s", file_name);
+    printf("Student: %s, number %d", s.name, s.number);
     return 0;
 }
---
+++ student.h
@@ -0,0 +1,9 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+struct student_t {
+    char* name;
+    int number;
+};
+
+#endif

36fc0d86b0d28ddc61e36cf0b4b1f895683bf98d
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-24 12:50:16 +0200

    modification of 5-1-6 make all inner structures as pointers

--- main.c
+++ main.c
@@ -7,14 +7,14 @@ struct point_t {
 };
 
 struct circle_t {
-    struct point_t center;
+    struct point_t* center;
     int radius;
 };
 
 struct triangle_t {
-    struct point_t A;
-    struct point_t B;
-    struct point_t C;
+    struct point_t* A;
+    struct point_t* B;
+    struct point_t* C;
 };
 
 enum figure_types {
@@ -25,101 +25,99 @@ enum figure_types {
 struct figure_t {
     enum figure_types type;
     union {
-        struct triangle_t triangle;
-        struct circle_t circle;
+        struct triangle_t* triangle;
+        struct circle_t* circle;
     };
 };
 
 void display_triangle(struct triangle_t* triangle) {
     printf("Triangle. A(%d %d), B(%d %d), C(%d %d)\n",
-           triangle->A.x, triangle->A.y, triangle->B.x, triangle->B.y, triangle->C.x, triangle->C.y);
+           triangle->A->x, triangle->A->y, triangle->B->x, triangle->B->y, triangle->C->x, triangle->C->y);
 }
 
 void display_circle(struct circle_t* circle) {
     printf("Circle. O(%d %d), R(%d)\n",
-           circle->center.x, circle->center.y, circle->radius);
+           circle->center->x, circle->center->y, circle->radius);
 }
 
 void display_figure(struct figure_t* f) {
     if (f->type == CIRCLE) {
-        display_circle(&(f->circle));
+        display_circle(f->circle);
     } else if (f->type == TRIANGLE) {
-        display_triangle(&(f->triangle));
+        display_triangle(f->triangle);
+    }
+}
+
+struct figure_t* create_figure(enum figure_types type) {
+    struct figure_t* f = malloc(sizeof(struct figure_t));
+    if (f == NULL) {
+        return NULL;
+    }
+    f->type = type;
+
+    if (type == CIRCLE) {
+        f->circle = malloc(sizeof(struct circle_t));
+        if (f->circle == NULL) {
+            free(f);
+            return NULL;
+        }
+
+        f->circle->center = malloc(sizeof(struct point_t));
+        if (f->circle->center == NULL) {
+            free(f->circle);
+            free(f);
+            return NULL;
+        }
+    } else if (type == TRIANGLE) {
+        f->triangle = malloc(sizeof(struct triangle_t));
+        if (f->triangle == NULL) {
+            free(f);
+            return NULL;
+        }
+
+        f->triangle->A = malloc(sizeof(struct point_t));
+        if (f->triangle->A == NULL) {
+            free(f->triangle);
+            free(f);
+            return NULL;
+        }
+        f->triangle->B = malloc(sizeof(struct point_t));
+        if (f->triangle->B == NULL) {
+            free(f->triangle->A);
+            free(f->triangle);
+            free(f);
+            return NULL;
+        }
+        f->triangle->C = malloc(sizeof(struct point_t));
+        if (f->triangle->C == NULL) {
+            free(f->triangle->A);
+            free(f->triangle->B);
+            free(f->triangle);
+            free(f);
+            return NULL;
+        }
+    }
+    return f;
+}
+
+void delete_figure(struct figure_t* f) {
+    if (f == NULL) {
+        return;
+    }
+    if (f->type == CIRCLE) {
+        free(f->circle->center);
+        free(f->circle);
+        free(f);
+    } else if (f->type == TRIANGLE) {
+        free(f->triangle->A);
+        free(f->triangle->B);
+        free(f->triangle->C);
+        free(f->triangle);
+        free(f);
     }
 }
 
 int main() {
-    // создаем структуру (не как указатель)
-    struct figure_t f1;
-    f1.type = CIRCLE;
-    f1.circle.center.x = 10;
-    f1.circle.center.y = 20;
-    f1.circle.radius = 5;
-    // отображаем окружность как фигуру
-    display_figure(&f1);
-    // отображаем окружность из фигуры как окружность
-    display_circle(&(f1.circle));
-
-    // отображаем треугольник из фигуры, в которой хранится окружность, для прикола ;)
-    display_triangle(&(f1.triangle));
-    // говорим, что фигура - треугольник, хотя по факту там хранится окружность, тоже для прикола ;)
-    f1.type = TRIANGLE;
-    // и теперь отображаем эту фигуру (треугольник), в которой по факту хранится окружность
-    display_figure(&f1);
-
-    printf("========================\n");
-
-    // создаем структуру, как указатель
-    struct figure_t* f2_p = malloc(sizeof(struct figure_t));
-    if (f2_p == NULL) {
-        printf("Failed to allocate memory\n");
-        return 1;
-    }
-
-    f2_p->type = TRIANGLE;
-    f2_p->triangle.A.x = 1;
-    f2_p->triangle.A.y = 2;
-    f2_p->triangle.B.x = 3;
-    f2_p->triangle.B.y = 4;
-    f2_p->triangle.C.x = 5;
-    f2_p->triangle.C.y = 6;
-    display_figure(f2_p);
-
-    // выводим треугольник, как окружность
-    display_circle(&(f2_p->circle));
-
-    free(f2_p);
-
-    printf("========================\n");
-
-    // создаем массив из 2-х фигур (элементом массива будет сама структура "фигура", а не указатель на фигуру)
-    struct figure_t* f_arr = malloc(sizeof(struct figure_t) * 2);
-    if (f_arr == NULL) {
-        printf("Failed to allocate memory\n");
-        return 1;
-    }
-
-    // инициализируем первый элемент массивва
-    f_arr->type = CIRCLE;
-    f_arr->circle.center.x = 8;
-    f_arr->circle.center.y = 12;
-    f_arr->circle.radius = 7;
-    // инициализируем второй элемент массивва
-    (f_arr+1)->type = TRIANGLE;
-    (f_arr+1)->triangle.A.x = 10;
-    (f_arr+1)->triangle.A.y = 20;
-    (f_arr+1)->triangle.B.x = 30;
-    (f_arr+1)->triangle.B.y = 40;
-    (f_arr+1)->triangle.C.x = 50;
-    (f_arr+1)->triangle.C.y = 60;
-
-    for (int i = 0; i < 2; i++) {
-        display_figure((f_arr+i));
-    }
-    free(f_arr);
-
-    printf("========================\n");
-
     // создаем массив из 2-х указателей на фигуры (элементом массива будет указатель на фигуру)
     struct figure_t** f_arr_p = malloc(sizeof(struct figure_t*) * 2);
     if (f_arr_p == NULL) {
@@ -128,31 +126,31 @@ int main() {
     }
 
     // инициализируем первый элемент массивва
-    *f_arr_p = malloc(sizeof(struct figure_t));
+    *f_arr_p = create_figure(CIRCLE);
     if (*f_arr_p == NULL) {
         free(f_arr_p);
         printf("Failed to allocate memory\n");
         return 1;
     }
-    (*f_arr_p)->type = CIRCLE;
-    (*f_arr_p)->circle.center.x = 80;
-    (*f_arr_p)->circle.center.y = 21;
-    (*f_arr_p)->circle.radius = 15;
+    (*f_arr_p)->circle->center->x = 80;
+    (*f_arr_p)->circle->center->y = 21;
+    (*f_arr_p)->circle->radius = 15;
+
     // инициализируем второй элемент массивва
-    *(f_arr_p+1) = malloc(sizeof(struct figure_t));
+    *(f_arr_p+1) = create_figure(TRIANGLE);
     if (*(f_arr_p+1) == NULL) {
-        free(*f_arr_p);
+        delete_figure(*f_arr_p);
         free(f_arr_p);
         printf("Failed to allocate memory\n");
         return 1;
     }
     (*(f_arr_p+1))->type = TRIANGLE;
-    (*(f_arr_p+1))->triangle.A.x = 15;
-    (*(f_arr_p+1))->triangle.A.y = 25;
-    (*(f_arr_p+1))->triangle.B.x = 35;
-    (*(f_arr_p+1))->triangle.B.y = 45;
-    (*(f_arr_p+1))->triangle.C.x = 55;
-    (*(f_arr_p+1))->triangle.C.y = 65;
+    (*(f_arr_p+1))->triangle->A->x = 15;
+    (*(f_arr_p+1))->triangle->A->y = 25;
+    (*(f_arr_p+1))->triangle->B->x = 35;
+    (*(f_arr_p+1))->triangle->B->y = 45;
+    (*(f_arr_p+1))->triangle->C->x = 55;
+    (*(f_arr_p+1))->triangle->C->y = 65;
 
     for (int i = 0; i < 2; i++) {
         display_figure(*(f_arr_p+i));
@@ -160,7 +158,7 @@ int main() {
 
     // чистим память каждого єлемента массива
     for (int i = 0; i < 2; i++) {
-        free(*(f_arr_p+i));
+        delete_figure(*(f_arr_p+i));
     }
     // чистим память всего массива
     free(f_arr_p);

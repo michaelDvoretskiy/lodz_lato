c997b48fc3cd240a83c4652e4b36c981afeb1018
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-25 08:55:53 +0200

    read line from the file  and the devide it to separate parts

--- main.c
+++ main.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 1000
 
 int main() {
     char file_name[31];
     printf("Enter name:\n");
-    scanf("%30s", file_name);
+    int scan_res = scanf("%30s", file_name);
+    if (scan_res != 1) {
+        printf("smth went wrong!\n");
+        return 1;
+    }
 
-    printf("You entered: %s", file_name);
+    FILE* f = fopen(file_name, "r");
+    if (f == NULL) {
+        printf("smth went wrong!\n");
+        return 1;
+    }
+
+    char* string_from_file = malloc(MAX_LINE_LENGTH);
+    while (fgets(string_from_file, MAX_LINE_LENGTH, f) != NULL) {
+        printf("String: %s\n", string_from_file);
+        char* sub_string_from_file = strtok(string_from_file, " \n");
+        while(sub_string_from_file != NULL) {
+            printf("Substring: %s\n", sub_string_from_file);
+            sub_string_from_file = strtok(NULL, " \n");
+        }
+        printf("================\n");
+    }
+
+    fclose(f);
+
     return 0;
 }

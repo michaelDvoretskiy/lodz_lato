afdcc6708d1a60bfcf6b119a0b88e58336ea890e
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-29 20:14:05 +0200

    pass pointer to the function

--- main.c
+++ main.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void set_value(int* number_pointer, int number_value) {
+    if (number_pointer == NULL) {
+        return;
+    }
+    *number_pointer = number_value;
+}
+
+void show_number(int* number_pointer) {
+    printf("%d\n", *number_pointer);
+}
 
 int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    scanf("%30s", file_name);
+    int* num_p;
+    num_p = malloc(sizeof(int));
+    if (num_p == NULL) {
+        return 1;
+    }
+    set_value(num_p, 10);
+    show_number(num_p);
+    free(num_p);
 
-    printf("You entered: %s", file_name);
+    int num_v;
+    set_value(&num_v, 20);
+    show_number(&num_v);
+
     return 0;
 }

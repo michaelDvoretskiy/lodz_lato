a14bf3540f0967204af0115fea93389a44117d91
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-29 20:58:58 +0200

    pass pointer for array of int to the function

--- main.c
+++ main.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-void set_value(int* number_pointer, int number_value) {
-    if (number_pointer == NULL) {
+void set_values(int* numbers, int arr_size) {
+    if (numbers == NULL) {
         return;
     }
-    *number_pointer = number_value;
+    for (int i = 0; i < arr_size; i++) {
+        *(numbers + i) = rand() % 1001;
+    }
 }
 
-void show_number(int* number_pointer) {
-    printf("%d\n", *number_pointer);
+void show_numbers(int* numbers, int arr_size) {
+    for (int i = 0; i < arr_size; i++) {
+        printf("%d ", *(numbers + i));
+    }
+    printf("\n");
 }
 
 int main() {
-    int* num_p;
-    num_p = malloc(sizeof(int));
-    if (num_p == NULL) {
+    srand(time(NULL));
+
+    int num_arr_size = 5;
+    int* num_arr = malloc(sizeof(int) * num_arr_size);
+    if (num_arr == NULL) {
         return 1;
     }
-    set_value(num_p, 10);
-    show_number(num_p);
-    free(num_p);
+    set_values(num_arr, num_arr_size);
+    show_numbers(num_arr, num_arr_size);
+    free(num_arr);
 
-    int num_v;
-    set_value(&num_v, 20);
-    show_number(&num_v);
+    printf("================\n");
+
+    int numbers2[num_arr_size];
+    set_values(numbers2, num_arr_size);
+    show_numbers(numbers2, num_arr_size);
 
     return 0;
 }

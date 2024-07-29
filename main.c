c07893416886456f173f4c845f247921471c96c9
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-29 20:36:58 +0200

    allocate memory for array of int

--- main.c
+++ main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main() {
-    int* num;
-    num = malloc(sizeof(int));
+    int arr_size = 5;
+
+    int* num = malloc(sizeof(int) * arr_size);
     if (num == NULL) {
         return 1;
     }
-    *num = 10;
-    printf("%d\n", *num);
+
+    for (int i = 0; i < arr_size; i++) {
+        *(num + i) = (i + 1) * 10;
+    }
+
+    for (int i = 0; i < arr_size; i++) {
+        printf("%d ", *(num + i));
+    }
+    printf("\n");
+
     free(num);
     return 0;
 }

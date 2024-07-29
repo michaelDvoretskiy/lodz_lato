7fcdc50aa6ea3040ed2cebeb789eb6baff1412ba
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-29 20:29:59 +0200

    allocate memory inside the function

--- main.c
+++ main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int main() {
+int* create_number(int value) {
     int* num;
     num = malloc(sizeof(int));
     if (num == NULL) {
+        return NULL;
+    }
+    *num = value;
+
+    return num;
+}
+
+int main() {
+    int* num = create_number(10);
+    if (num == NULL) {
         return 1;
     }
-    *num = 10;
     printf("%d\n", *num);
     free(num);
     return 0;

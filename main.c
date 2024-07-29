dcd644f167b2c1e966d467a573ee788d33925bd1
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-29 20:15:17 +0200

    create pointer, allocate memory, free memory

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    scanf("%30s", file_name);
-
-    printf("You entered: %s", file_name);
+    int* num;
+    num = malloc(sizeof(int));
+    if (num == NULL) {
+        return 1;
+    }
+    *num = 10;
+    printf("%d\n", *num);
+    free(num);
     return 0;
 }

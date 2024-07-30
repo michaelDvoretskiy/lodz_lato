5602863ae7625c4a62fc7fe2c5942a276fd77b6b
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-30 17:37:43 +0200

    use constant to define array size

--- main.c
+++ main.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-void set_values(int* numbers, int arr_size) {
+#define ARR_SIZE 5
+
+void set_values(int* numbers) {
     if (numbers == NULL) {
         return;
     }
-    for (int i = 0; i < arr_size; i++) {
+    for (int i = 0; i < ARR_SIZE; i++) {
         *(numbers + i) = rand() % 1001;
     }
 }
 
-void show_numbers(int* numbers, int arr_size) {
-    for (int i = 0; i < arr_size; i++) {
+void show_numbers(int* numbers) {
+    for (int i = 0; i < ARR_SIZE; i++) {
         printf("%d ", *(numbers + i));
     }
     printf("\n");
@@ -21,20 +23,19 @@ void show_numbers(int* numbers, int arr_size) {
 int main() {
     srand(time(NULL));
 
-    int num_arr_size = 5;
-    int* num_arr = malloc(sizeof(int) * num_arr_size);
+    int* num_arr = malloc(sizeof(int) * ARR_SIZE);
     if (num_arr == NULL) {
         return 1;
     }
-    set_values(num_arr, num_arr_size);
-    show_numbers(num_arr, num_arr_size);
+    set_values(num_arr);
+    show_numbers(num_arr);
     free(num_arr);
 
     printf("================\n");
 
-    int numbers2[num_arr_size];
-    set_values(numbers2, num_arr_size);
-    show_numbers(numbers2, num_arr_size);
+    int numbers2[ARR_SIZE];
+    set_values(numbers2);
+    show_numbers(numbers2);
 
     return 0;
 }

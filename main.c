c0f5c2750a71a347bc3c1f33ddda429e565d909b
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-30 17:44:13 +0200

    allocate memory for array inside a function and return it as a result

--- main.c
+++ main.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int* create_number(int value) {
+int* create_arr(int size) {
     int* num;
-    num = malloc(sizeof(int));
+    num = malloc(sizeof(int) * size);
     if (num == NULL) {
         return NULL;
     }
-    *num = value;
+    for (int i = 0; i < size; i++) {
+        *(num + i) = rand() % 1001;
+    }
 
     return num;
 }
 
+void show_numbers(int* numbers, int arr_size) {
+    for (int i = 0; i < arr_size; i++) {
+        printf("%d ", *(numbers + i));
+    }
+    printf("\n");
+}
+
+
 int main() {
-    int* num = create_number(10);
-    if (num == NULL) {
+    srand(time(NULL));
+    int* nums = create_arr(10);
+    if (nums == NULL) {
         return 1;
     }
-    printf("%d\n", *num);
-    free(num);
+    show_numbers(nums, 10);
+    free(nums);
     return 0;
 }

7281036eb6796994e93026fcd7c84c44d122775a
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-07-31 17:33:29 +0200

    allocate memory for an array of numbers and return it as a function parameter

--- main.c
+++ main.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void create_nums(int** nums_p, int arr_size) {
+    if (nums_p == NULL) {
+        return;
+    }
+    *nums_p = malloc(sizeof(int) * arr_size);
+    if (*nums_p == NULL) {
+        return;
+    }
+    for (int i = 0; i < arr_size; i++) {
+        *(*nums_p + i) = rand() % 1001;
+    }
+}
+
+void show_nums(int* numbers, int arr_size) {
+    for (int i = 0; i < arr_size; i++) {
+        printf("%d ", *(numbers + i));
+    }
+    printf("\n");
+}
+
+void destroy_nums(int** nums_p, int arr_size) {
+    if (nums_p == NULL) {
+        return;
+    }
+    if (*nums_p != NULL) {
+        free(*nums_p);
+    }
+    free(nums_p);
+}
 
 int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    scanf("%30s", file_name);
-
-    printf("You entered: %s", file_name);
+    int** numbers_pointer = malloc(sizeof(int*));
+    create_nums(numbers_pointer, 10);
+    show_nums(*numbers_pointer, 10);
+    destroy_nums(numbers_pointer, 10);
     return 0;
 }

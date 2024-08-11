993675eee2a529a20f68f52b1feb4627628b6c64
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-08-11 10:40:08 +0200

    reverse int array

--- main.c
+++ main.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void show_arr(int* arr, int size) {
+    if (arr == NULL || size <= 0) {
+        return;
+    }
+    for (int i = 0; i < size; i++) {
+        printf("%hd ", *(arr + i));
+    }
+    printf("\n");
+    printf("==============\n");
+}
+
+void reverse(int* arr, int size) {
+    int* first = arr;
+    int* last = arr + size - 1;
+
+    while (first < last) {
+        int tmp = *first;
+        *first = *last;
+        *last = tmp;
+
+        first++;
+        last--;
+    }
+}
 
 int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    scanf("%30s", file_name);
+    int* arr = malloc(sizeof(int) * 10);
+    if (arr == NULL) {
+        return 1;
+    }
+    for (int i = 0; i < 10; i++) {
+        *(arr + i) = i;
+    }
+    show_arr(arr, 10);
 
-    printf("You entered: %s", file_name);
+    reverse(arr, 10);
+    show_arr(arr, 10);
+
     return 0;
 }

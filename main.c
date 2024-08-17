b2a59fca7a5d828ada7ce793582c6d6a5dab4713
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-08-17 18:17:47 +0200

    one string

--- main.c
+++ main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    char file_name[31];
-    printf("Enter name:\n");
-    scanf("%30s", file_name);
+    int max_len = 20;
+    char* str = malloc(sizeof(char) * (max_len + 1));
 
-    printf("You entered: %s", file_name);
+    printf("Enter string:\n");
+    char chr;
+    if (scanf("%c", &chr) != 1) {
+        printf("Input error");
+        return 1;
+    }
+    int i = 0;
+    while (chr != '\n') {
+        if (i < max_len) {
+            *(str + i) = chr;
+            i++;
+        }
+        if (scanf("%c", &chr) != 1) {
+            printf("Intup error");
+            return 1;
+        }
+    }
+    *(str + i) = '\0';
+
+    printf("You entered : %s\n", str);
+
+    free(str);
+
     return 0;
 }

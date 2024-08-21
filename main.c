e11f5a2ae40713898af832151c30d0d36239b8d1
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-08-21 17:08:25 +0200

    array of pointers to the functions

--- main.c
+++ main.c
@@ -1,17 +1,36 @@
+#include <stdio.h>
 #include "functions.h"
 
 int main() {
-    int (*operation)(int, int);
-    operation = sub;
+    int (*operations[2])(int, int) = {add, sub};
+    void (*showers[2])(int) = {show, show2};
 
-    void (*shower)(int) = show2;
+    int operation_number;
+    printf("Choose operation (0 - add, 1 - sub):");
+    if (scanf("%d", &operation_number) != 1) {
+        return 1;
+    }
+    if (operation_number != 0 && operation_number != 1) {
+        return 1;
+    }
 
-    int res = operation(5, 10);
-    shower(res);
+    int x, y;
+    printf("Enter two numbers:");
+    if (scanf("%d %d", &x, &y) != 2) {
+        return 1;
+    }
 
-    operation = add;
-    shower = show;
-    shower(operation(3, 5));
+    int show_type;
+    printf("Choose how to display the result (0 - short, 1 - full):");
+    if (scanf("%d", &show_type) != 1) {
+        return 1;
+    }
+    if (show_type != 0 && show_type != 1) {
+        return 1;
+    }
+
+    int res = operations[show_type](x,y);
+    showers[show_type](res);
 
     return 0;
 }

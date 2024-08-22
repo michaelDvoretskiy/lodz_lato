d1bb36795ecf4109a14ed58b4dc753bcac268d2e
michaelDvoretskiy <m.dvoretskiy@gmail.com>
2024-08-22 12:23:39 +0200

    void* in finctions signature

--- main.c
+++ main.c
@@ -6,55 +6,57 @@ struct point_t {
     int y;
 };
 
-int add_int(int x, int y) {
-    return x + y;
+void* add_int(const void* x, const void* y) {
+    int* result = malloc(sizeof(int));
+    if (!result) {
+        return NULL;
+    }
+
+    *result = *((int*)x) + *((int*)y);
+
+    return result;
 }
 
-float add_float(float x, float y) {
-    return x + y;
+void* add_float(const void* x, const void* y) {
+    float* result = malloc(sizeof(float));
+    if (!result) {
+        return NULL;
+    }
+
+    *result = *((float*)x) + *((float*)y);
+
+    return result;
 }
 
-struct point_t* add_point(struct point_t* p1, struct point_t* p2) {
+const void* add_point(const void* p1, const void* p2) {
     struct point_t* new_point = malloc(sizeof(struct point_t));
-    new_point->x = p1->x + p2->x;
-    new_point->y = p1->y + p2->y;
+    new_point->x = ((struct point_t*)p1)->x + ((struct point_t*)p2)->x;
+    new_point->y = ((struct point_t*)p1)->y + ((struct point_t*)p2)->y;
 
     return new_point;
 }
 
 int main() {
-    int (*add)(int, int);
+    void* (*add)(const void*, const void*);
+
+    int x_int = 10, y_int = 20;
     add = add_int;
-    printf("%d\n", add(10, 20));
+    int* res_int = add((const void*)(&x_int), (const void*)(&y_int));
+    printf("%d\n", *res_int);
+    free(res_int);
 
-    // дальнейшая проблема в том, что мы теперь не можем использовать указатель add
-    // чтобы записать туда add_float или add_point т.к. у них разный тип (разная сигнатура)
-    // add_int - int (int, int)
-    // add_float - float (float, float)
-    // add_point - struct point_t* (struct point_t*, struct point_t*)
+    float x_float = 10.5f, y_float = 2.05f;
+    add = add_float;
+    float* res_float = add((const void*)(&x_float), (const void*)(&y_float));
+    printf("%f\n", *res_float);
+    free(res_float);
 
-    // нужно создавать новые переменные-указатели для float и point
-    // и работать через них
-    float (*add2)(float, float) = add_float;
-    printf("%f\n", add2(10.5f, 2.05f));
-
-    struct point_t* (*add3)(struct point_t*, struct point_t*) = add_point;
     struct point_t p1; p1.x = 10; p1.y = 20;
     struct point_t p2; p2.x = 15; p2.y = -7;
-    struct point_t* p = add3(&p1, &p2);
-    printf("%d %d\n", p->x, p->y);
-
-    // но тогда вопрос, на кой нам тут указатели add, add2 и add3,
-    // если можно просто так
-    printf("%d\n", add_int(10, 20));
-    printf("%f\n", add_float(10.5f, 2.05f));
-    struct point_t* pp = add_point(&p1, &p2);
-    printf("%d %d\n", pp->x, pp->y);
-
-    // было бы удобно создать такой указатель add, чтобы туда в зависимости от того,
-    // с каким типом мы выберем работать, передать add_int, add_float или add_point
-    // и работать потом с ним
-    // но мы не можем этого сделать, пока у add_int, add_float или add_point разные сигнатуры
+    add = add_point;
+    struct point_t* res_point = add(&p1, &p2);
+    printf("%d %d\n", res_point->x, res_point->y);
+    free(res_point);
 
     return 0;
 }
